cbb0604a3d4ce14b42c0995698d2ca66c1e8f2e5
iemttt <s02190454@gse.cs.msu.ru>
2024-02-21 15:33:02 +0300

    Add preset support
    supported presets: -bd

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -2,9 +2,20 @@ from cowsay import cowsay
 
 import argparse
 
+PRESET_OPTIONS = {
+    "b", "d"
+}
+
+def get_preset(args):
+    return "".join(opt for opt in PRESET_OPTIONS if getattr(args, opt, False))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("message", type=str)
+    parser.add_argument("-b", help="Borg mode", action="store_true")
+    parser.add_argument("-d", help="Dead mode", action="store_true")
     args = parser.parse_args()
+    preset = get_preset(args)
     message = args.message
-    print(cowsay(message, preset="l"))
+
+    print(cowsay(message, preset=preset))
