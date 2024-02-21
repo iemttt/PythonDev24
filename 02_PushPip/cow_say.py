444c913ceccc95fd1d661af6d2fca7012b266b1c
iemttt <s02190454@gse.cs.msu.ru>
2024-02-21 16:22:30 +0300

    Add option to read message from stdin

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,6 +1,8 @@
 from cowsay import cowsay, Option
 
 import argparse
+from sys import stdin
+
 
 PRESET_OPTIONS = {
     "b", "d", "g", "p", "s", "t", "w", "y"
@@ -23,15 +25,19 @@ if __name__ == '__main__':
     parser.add_argument("-y", help="Brings on the cow's youthful appearance", action="store_true")
     parser.add_argument("-W", help="specifies roughly where the message should be wrapped. The default is equivalent to -W 40 i.e. wrap words at or before the 40th column.", type=int)
     parser.add_argument("-n", help="If it is specified, the given message will not be word-wrapped", action="store_true")
-    parser.add_argument("message", type=str)
-
+    parser.add_argument("message", type=str, nargs="?")
     args = parser.parse_args()
+    
+    if args.message is None:
+        message = stdin.read()
+    else:    
+        message = args.message
+
     preset = get_preset(args)
     eyes = args.e[:2] if args.e else Option.eyes
     tongue = args.T[:2] if args.T else Option.tongue
     width = args.W if args.W else 40
     wrap_text = args.n
-    message = args.message
     
     print(cowsay(
         message=message,
