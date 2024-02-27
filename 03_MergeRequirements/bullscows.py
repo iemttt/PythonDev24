f6cff83c6d7e4b8b733144a9f5e26ee4cbb27a81
iemttt <s02190454@gse.cs.msu.ru>
2024-02-27 16:49:31 +0300

    Add gameplay loop

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,4 +1,5 @@
-from typing import Tuple, Set, List, Optional
+from typing import Tuple, Set, List, Optional, Callable
+from random import choice
 
 def bullscows(guess: str, secret: str) -> Tuple[int, int]:
     bulls: int = 0
@@ -25,3 +26,28 @@ def ask(prompt: str, valid: List[str] = None) -> str:
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
     print(format_string.format(bulls, cows))
+
+
+def gameplay(ask: Callable, inform: Callable, words: List[str]) -> int:
+    # Задумывает случайное слово из списка слов words: list[str]
+    secret: str = choice(words)
+    guess: Optional[str] = None
+    tries: int = 0
+    while guess != secret:
+        tries += 1
+        #Спрашивает у пользователя слово с помощью функции ask("Введите слово: ", words)
+        guess: str = ask("Введите слово: ", words)
+        #Выводит пользователю результат с помощью функции inform("Быки: {}, Коровы: {}", b, c)
+        inform("Быков: {}, Коров: {}", *bullscows(guess, secret))
+        #Если слово не отгадано, переходит к п. 1
+
+    #Если слово отгадано, возвращает количество попыток — вызовов ask() 
+    print("Количество попыток:", tries)
+
+WORDS: List[str] = list(set((
+    "ропот",
+    "полип",
+    "топор"
+)))
+
+gameplay(ask, inform, WORDS)
