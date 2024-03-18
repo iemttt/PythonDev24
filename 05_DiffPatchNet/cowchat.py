76238fc20b166722d2653592ad8201cfba55fd63
iemttt <s02190454@gse.cs.msu.ru>
2024-03-18 14:22:24 +0300

    Add Client class

--- 05_DiffPatchNet/cowchat.py
+++ 05_DiffPatchNet/cowchat.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
 import asyncio
 
+
+class Client():
+    def __init__(self, peername):
+        self.peername = peername
+        self.queue = asyncio.Queue()
+        self.login = None
+
 clients = {}
 
 async def chat(reader, writer):
     me = "{}:{}".format(*writer.get_extra_info('peername'))
     print(me)
-    clients[me] = asyncio.Queue()
+    clients[me] = Client(me)
     send = asyncio.create_task(reader.readline())
-    receive = asyncio.create_task(clients[me].get())
+    receive = asyncio.create_task(clients[me].queue.get())
     while not reader.at_eof():
         done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        print(done)
         for q in done:
             if q is send:
                 send = asyncio.create_task(reader.readline())
-                for out in clients.values():
-                    if out is not clients[me]:
-                        await out.put(f"{me} {q.result().decode().strip()}")
+                for peer in clients:
+                    if peer is not me:
+                        await clients[peer].queue.put(f"{me} {q.result().decode().strip()}")
             elif q is receive:
-                receive = asyncio.create_task(clients[me].get())
+                receive = asyncio.create_task(clients[me].queue.get())
                 writer.write(f"{q.result()}\n".encode())
                 await writer.drain()
     send.cancel()
